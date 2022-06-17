1a6682932df8d42865882ef5eb946e8351eed77d
Olivia17111 <99030869+Olivia17111@users.noreply.github.com>
2022-06-17 17:09:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    clown_mustache = loadImage('');
+  }
+  
+  function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+  }  
+
+  function draw() {
+    image(video, 0, 0, 300, 300);
+    
+  }
